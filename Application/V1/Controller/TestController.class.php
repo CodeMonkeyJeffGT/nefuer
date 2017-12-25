03d8f52084755f21201c0c5a513eab402e512952
CodeMonkeyJeffGT <18846938586@163.com>
2017-12-25 21:28:42 +0800

    增加考试查询

--- Application/V1/Controller/TestController.class.php
+++ Application/V1/Controller/TestController.class.php
@@ -6,193 +6,28 @@ class TestController extends ApiController {
     public function index()
     {
         $nefuer = \Nefu::getInstance($this->payload['user']['account'], $this->payload['user']['password'], $this->payload['user']['cookie']);
-        $score = $nefuer->userTest();
-        if(FALSE === $score)
+
+        $grade = substr($this->payload['user']['account'], 0, 4);
+        $year = date('Y');
+        if(date('m') < 9)
+        	$year--;
+
+        $term = array();
+        for($i = $grade; $i < $year; $i++)
         {
-            $this->apiError('获取成绩失败，请重新登录或查看教务系统是否可用');
+        	$term[$i . '-1'] = $nefuer->userTest($i . '-' . ($i + 1) . '-1');
+	        if(FALSE === $term[$i . '-1'])
+	        {
+	            $this->apiError('获取考试失败，请重新登录或查看教务系统是否可用');
+	        }
+        	$term[$i . '-2'] = $nefuer->userTest($i . '-' . ($i + 1) . '-2');
+	        if(FALSE === $term[$i . '-2'])
+	        {
+	            $this->apiError('获取考试失败，请重新登录或查看教务系统是否可用');
+	        }
         }
+        $test = $nefuer->userTest();
 
-        $term = array(
-            'score' => array(),
-            'count' => array(),
-            'avg'   => array()
-        );
-
-        $scoreAllAdd = array();
-        for($i = 0, $iloop = count($score['item']); $i < $iloop; $i++)
-        {
-            $index = -1;
-            for($j = 0,$jloop = count($score['all']); $j < $jloop; $j++)
-            {
-                if($score['item'][$i]['code'] === $score['all'][$j]['code'] && $score['item'][$i]['term'] === $score['all'][$j]['term'])
-                {
-                    $index = $j;
-                    break;
-                }
-            }
-            if($index === -1)
-            {
-                $scoreAllAdd[] = array(
-                    'term'      => $score['item'][$i]['term'],
-                    'name'      => $score['item'][$i]['name'],
-                    'score'     => '（阶段）',
-                    'type'      => $score['item'][$i]['type'],
-                    'num'       => $score['item'][$i]['num'],
-                    'attribute' => $score['item'][$i]['attribute'],
-                    'children'  => array()
-                );
-                if('' != $score['item'][$i]['s_1'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '阶段1',
-                        'score' => $score['item'][$i]['s_1']
-                    );
-                if('' != $score['item'][$i]['s_2'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '阶段2',
-                        'score' => $score['item'][$i]['s_2']
-                    );
-                if('' != $score['item'][$i]['s_3'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '阶段3',
-                        'score' => $score['item'][$i]['s_3']
-                    );
-                if('' != $score['item'][$i]['s_4'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '阶段4',
-                        'score' => $score['item'][$i]['s_4']
-                    );
-                if('' != $score['item'][$i]['s_5'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '阶段5',
-                        'score' => $score['item'][$i]['s_5']
-                    );
-                if('' != $score['item'][$i]['s_common'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '平时成绩',
-                        'score' => $score['item'][$i]['s_common']
-                    );
-                if('' != $score['item'][$i]['s_middle'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '期中',
-                        'score' => $score['item'][$i]['s_middle']
-                    );
-                if('' != $score['item'][$i]['s_final'])
-                    $scoreAllAdd[count($scoreAllAdd) - 1]['children'][] = array(
-                        'name' => '期末',
-                        'score' => $score['item'][$i]['s_final']
-                    );
-            }
-            else
-            {
-                if( ! isset($score['all'][$index]['children']))
-                    $score['all'][$index]['children'] = array();
-
-                if('' != $score['item'][$i]['s_1'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '阶段1',
-                        'score' => $score['item'][$i]['s_1']
-                    );
-                if('' != $score['item'][$i]['s_2'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '阶段2',
-                        'score' => $score['item'][$i]['s_2']
-                    );
-                if('' != $score['item'][$i]['s_3'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '阶段3',
-                        'score' => $score['item'][$i]['s_3']
-                    );
-                if('' != $score['item'][$i]['s_4'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '阶段4',
-                        'score' => $score['item'][$i]['s_4']
-                    );
-                if('' != $score['item'][$i]['s_5'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '阶段5',
-                        'score' => $score['item'][$i]['s_5']
-                    );
-                if('' != $score['item'][$i]['s_common'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '平时成绩',
-                        'score' => $score['item'][$i]['s_common']
-                    );
-                if('' != $score['item'][$i]['s_middle'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '期中',
-                        'score' => $score['item'][$i]['s_middle']
-                    );
-                if('' != $score['item'][$i]['s_final'])
-                    $score['all'][$index]['children'][] = array(
-                        'name' => '期末',
-                        'score' => $score['item'][$i]['s_final']
-                    );
-            }
-        }
-        $score = array_merge($score['all'], array_reverse($scoreAllAdd));
-
-        for($i = count($score) - 1; $i >= 0; $i--)
-        {
-            $score[$i]['term'] = substr($score[$i]['term'], 0, 4) . '-' . $score[$i]['term'][10];
-            if( ! isset($term['score'][$score[$i]['term']]))
-            {
-                $term['score'][$score[$i]['term']] = array();
-                $term['count'][$score[$i]['term']] = array(
-                    'allNum'    => 0,
-                    'allScore'  => 0,
-                    'baseNum'   => 0,
-                    'baseScore' => 0
-                );
-            }
-
-            if(is_numeric($score[$i]['score']))
-            {
-                if('公选' !== $score[$i]['attribute'])
-                {
-                    $term['count'][$score[$i]['term']]['baseNum'] += $score[$i]['num'];
-                    $term['count'][$score[$i]['term']]['baseScore'] += $score[$i]['score'] * $score[$i]['num'];
-                }
-                $term['count'][$score[$i]['term']]['allNum'] += $score[$i]['num'];
-                $term['count'][$score[$i]['term']]['allScore'] += $score[$i]['score'] * $score[$i]['num'];
-            }
-
-	    $term['score'][$score[$i]['term']][] = $score[$i];
-	    /**array(
-                'name'     => $score[$i]['name'],
-                'score'    => $score[$i]['score'],
-                'num'      => $score[$i]['num'],
-                'children' => $score[$i]['children'],
-	    );*/
-        }
-
-        foreach ($term['count'] as $key => $value)
-        {
-            $tmpTerm = substr($key, 0, 4);
-            $tmpKey = $key[5];
-            if( ! isset($term['avg'][$tmpTerm]))
-                $term['avg'][$tmpTerm] = array();
-            $term['avg'][$tmpTerm][$tmpKey] = array(
-                'all'  => $term['count'][$key]['allNum'] === 0 ? 0 : number_format($term['count'][$key]['allScore'] / $term['count'][$key]['allNum'], 2),
-                'base'  => $term['count'][$key]['baseNum'] === 0 ? 0 : number_format($term['count'][$key]['baseScore'] / $term['count'][$key]['baseNum'], 2)
-            );
-            if($term['avg'][$tmpTerm][$tmpKey]['all'] === 0 && $term['avg'][$tmpTerm][$tmpKey]['base'] === 0)
-                unset($term['avg'][$tmpTerm][$tmpKey]);
-        }
-
-        foreach ($term['avg'] as $key => $value)
-        {
-            if(isset($term['avg'][$key][1]) && isset($term['avg'][$key][2]))
-            {
-                $term['avg'][$key]['all'] = array(
-                    'all' => number_format(($term['count'][$key . '-1']['allScore'] + $term['count'][$key . '-2']['allScore']) / ($term['count'][$key . '-1']['allNum'] + $term['count'][$key . '-2']['allNum']), 2),
-                    'base' => number_format(($term['count'][$key . '-1']['baseScore'] + $term['count'][$key . '-2']['baseScore']) / ($term['count'][$key . '-1']['baseNum'] + $term['count'][$key . '-2']['baseNum']), 2)
-                );
-            }
-            if(empty($term['avg'][$key]))
-                unset($term['avg'][$key]);
-        }
-
-        unset($term['count']);
 
         $this->payload['user']['cookie'] = $nefuer->getCookie();
     	$this->apiSuccess($term);
