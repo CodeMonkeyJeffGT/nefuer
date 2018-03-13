769c66b1738b328272069bfc580c64f5be712484
CodeMonkeyJeffGT <18846938586@163.com>
2018-03-13 16:00:35 +0800

    增加数据库错误处理

--- FF/Core/Model.php
+++ FF/Core/Model.php
@@ -8,7 +8,7 @@ class Model extends \PDO {
 		$username = Config::get('DB_USERNAME');
 		$passwd = Config::get('DB_PASSWORD');
 		try{
-			parent::__construct($dsn, $username, $passwd);
+			parent::__construct($dsn, $username, $passwd, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "set names " . Config::get('DB_CHARSET')));
 		} catch(\PDOException $e) {
 			p($e->getMessage());
 		}
@@ -17,13 +17,17 @@ class Model extends \PDO {
 	public function query($sql, $param = array()) {
 		$st = $this->prepare($sql);
 		if ( ! is_array($param)) {
-			$st->bindParam(1, $param);
+			$st->bindValue(1, $param);
 		} else {
 			for ($i = 0, $iLoop = count($param); $i < $iLoop; $i++) {
-				$st->bindParam($i + 1, $param[$i]);
+				$st->bindValue($i + 1, $param[$i]);
 			}
 		}
 		$st->execute();
+		$error = $st->errorInfo();
+		if ($error['0'] !== '00000') {
+			throw new \Exception($error[2], 1);
+		}
 		$result = $st->fetchAll();
 		for ($i = 0, $iLoop = count($result); $i < $iLoop; $i++) {
 			for ($j = 0, $jLoop = count($result[$i]) / 2; $j < $jLoop; $j++) {
